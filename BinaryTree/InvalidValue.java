e4d58cd3d8444c8c5af0adaa37e447887e7854d4
Karen Baz <kbaz@correo.um.edu.uy>
2019-06-13 23:55:12 -0300

    mi commit

---
+++ BinaryTree/InvalidValue.java
@@ -0,0 +1,7 @@
+public class InvalidValue extends Exception {
+
+   public InvalidValue(String message) {
+        super(message);
+    }
+
+}
---
+++ Heap/Nodo.java
@@ -0,0 +1,20 @@
+package TADS.Heap;
+
+public class Nodo< K extends Comparable<K>,D>{
+
+    private K key;
+    private D dato;
+
+
+    public Nodo(K key, D dato) {
+        this.key = key;
+        this.dato = dato;
+    }
+    public K getKey() {
+        return key;
+    }
+
+    public D getDato() {
+        return dato;
+    }
+}
